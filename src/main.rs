fcd3a29e006b5903bb8aa3e8faa100674a11f310
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-08-17 08:56:35 +0900

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ mod ext;
 mod grep;
 mod sort;
 
+use clap::{Args, Parser};
 use grep::grep;
 use sort::process_file;
 
+#[derive(Debug, Parser)]
+#[command(name = "cargo", bin_name = "cargo")]
+enum Cli {
+    SortDerives(SortDerivesArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(version, about, long_about = None)]
+struct SortDerivesArgs {}
+
 fn main() {
+    let Cli::SortDerives(_args) = Cli::parse();
+
     for (file_path, line_numbers) in grep().unwrap() {
         process_file(&file_path, line_numbers).unwrap();
     }
