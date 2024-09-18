f5ef936f12ffb726d33fa70a2438289ac0582cf0
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-09-18 09:43:16 +0900

    Add grep tests

--- src/grep.rs
+++ src/grep.rs
@@ -97,3 +97,98 @@ fn grep_file(path: PathBuf, tx: &Sender<Result<Match, String>>) {
         tx.send(Err(err.to_string())).unwrap();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_fs::prelude::{FileWriteStr, PathChild};
+
+    use super::*;
+
+    #[test]
+    fn test_grep() {
+        let files = &[
+            ("a.rs", rs_file_1(), true),
+            ("b.rs", rs_file_1(), true),
+            ("c.rs", rs_file_2(), false), // no derive
+            ("x/xa.rs", rs_file_1(), true),
+            ("x/xb.txt", rs_file_1(), false), // not .rs
+            ("x/y/ya.rs", rs_file_1(), true),
+            ("x/z/za.rs", rs_file_1(), true),
+        ];
+        let exclude = vec![];
+
+        let tmp_root_dir = setup_tmp_files(files);
+        let expected = expected_matches(tmp_root_dir.path(), files);
+
+        let actual = grep(tmp_root_dir.path(), exclude).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_grep_with_exclude() {
+        let files = &[
+            ("a.rs", rs_file_1(), true),
+            ("b.rs", rs_file_1(), false),
+            ("x/xa.rs", rs_file_1(), false),
+            ("x/xb.rs", rs_file_1(), false),
+            ("x/y/ya.rs", rs_file_1(), false),
+            ("x/z/za.rs", rs_file_1(), false),
+            ("o/oa.rs", rs_file_1(), true),
+            ("o/p/pa.rs", rs_file_1(), false),
+            ("o/p/pb.rs", rs_file_1(), true),
+            ("k/l/m/n/na.rs", rs_file_1(), false),
+        ];
+        let exclude = vec![
+            "b.rs".into(),
+            "x/*".into(),
+            "pa.rs".into(),
+            "k/**/na.rs".into(),
+        ];
+
+        let tmp_root_dir = setup_tmp_files(files);
+        let expected = expected_matches(tmp_root_dir.path(), files);
+
+        let actual = grep(tmp_root_dir.path(), exclude).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    fn rs_file_1() -> (&'static str, HashSet<usize>) {
+        let source = r#"
+        #[derive(Debug)]
+        struct A;
+
+        #[derive(Clone, Copy)]
+        struct B;
+        "#;
+        let derive_lines = HashSet::from([2, 5]);
+        (source, derive_lines)
+    }
+
+    fn rs_file_2() -> (&'static str, HashSet<usize>) {
+        let source = r#"
+        struct A;
+        "#;
+        let derive_lines = HashSet::from([]);
+        (source, derive_lines)
+    }
+
+    type Files<'a> = &'a [(&'a str, (&'a str, HashSet<usize>), bool)];
+
+    fn setup_tmp_files(files: Files) -> assert_fs::TempDir {
+        let tmp_root_dir = assert_fs::TempDir::new().unwrap();
+        for (path, (content, _), _) in files.iter() {
+            tmp_root_dir.child(path).write_str(content).unwrap();
+        }
+        tmp_root_dir
+    }
+
+    fn expected_matches(tmp_root_path: &Path, files: Files) -> Matches {
+        files
+            .iter()
+            .filter(|(_, (_, _), is_match)| *is_match)
+            .map(|(p, (_, ls), _)| (tmp_root_path.join(p), ls.iter().copied().collect()))
+            .collect()
+    }
+}
