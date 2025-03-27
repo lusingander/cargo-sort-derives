ae65b86ac52f54b9b1c117495abfd633d5ad03bc
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-03-27 21:43:51 +0900

    Fix integration tests

--- fixtures/expected/default/x/xa.rs
+++ fixtures/expected/default/x/xa.rs
@@ -1,4 +1,18 @@
+use std::cmp;
+
 #[derive(Clone, serde::Deserialize, Eq, PartialEq, serde::Serialize)]
-struct Xa {
+struct Xa1 {
     s: String,
 }
+
+#[derive(Clone, std::fmt::Debug, cmp::Eq, std::cmp::PartialEq)]
+struct Xa2 {
+    i: i32,
+}
+
+// mod xa3 {
+//     #[derive(Clone, Copy, Eq, PartialEq)]
+//     pub struct Xa3 {
+//         f: f32,
+//     }
+// }
--- fixtures/input/x/xa.rs
+++ fixtures/input/x/xa.rs
@@ -1,4 +1,18 @@
+use std::cmp;
+
 #[derive(Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
-struct Xa {
+struct Xa1 {
     s: String,
 }
+
+#[derive(std::fmt::Debug, Clone, std::cmp::PartialEq, cmp::Eq)]
+struct Xa2 {
+    i: i32,
+}
+
+// mod xa3 {
+//     #[derive(Clone, Copy, PartialEq, Eq)]
+//     pub struct Xa3 {
+//         f: f32,
+//     }
+// }
