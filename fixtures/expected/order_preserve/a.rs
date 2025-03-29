631a442b7ce3e565d62f9fa0836e7b0faa95f9b2
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-03-29 09:40:29 +0900

    Fix integration test cases

--- fixtures/expected/order_preserve/a.rs
+++ fixtures/expected/order_preserve/a.rs
@@ -1,4 +1,20 @@
 #[derive(Default, Debug, Eq, PartialEq, Clone, Hash, Copy)]
-pub struct A {
+pub struct A1 {
     a: i32,
+}
+
+mod a {
+    // sort-derives-disable-next-line
+    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    struct A2 {
+        a: i32,
+    }
+
+    // ...
+    // sort-derives-disable-next-line
+    // ...
+    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    struct A3 {
+        a: i32,
+    }
 }
