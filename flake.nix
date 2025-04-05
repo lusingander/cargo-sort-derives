42a0d6241def9bf5d945805ce3b543347ed01520
valeratrades <valeratrades@gmail.com>
2025-04-05 20:40:41 +0000

    fix: _

--- flake.nix
+++ flake.nix
@@ -1,12 +1,29 @@
 {
-  pkgs ? import <nixpkgs> { },
-}:
-let
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-in
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = manifest.name;
-  version = manifest.version;
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        pname = manifest.name;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+          inherit pname;
+          version = manifest.version;
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      }
+    );
 }
